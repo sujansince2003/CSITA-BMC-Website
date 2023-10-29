ae910cb4a5996e3155ae1b7a2370b499415ac1b9
im.bipo <bipinkhatri.ram@gmail.com>
2023-10-29 19:11:10 +0545

    testimonials styling

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/autoplay";
 // Import Swiper styles
-import { FaQuoteLeft } from 'react-icons/fa';
+import { FaQuoteLeft } from "react-icons/fa";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Testidata } from "../../data";
@@ -15,68 +15,68 @@ SwiperCore.use([Autoplay]); //import swipercore to use autoplay
 const Testimonials = () => {
   return (
     <div className="myReview">
-      <div className="WellWishers">
-        <h2 className="reviews--heading">What our wellwishers say</h2>
-      </div>
-      <div>
-        <Swiper
-          slidesPerView={1}
-          loop={true}
-          autoplay={true} //For autoenabling the swiper functionality with infinite loop
-          speed={2000}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {Testidata.map((item, index) => {
-            //While rendering data on screen return the component with small brackets
-            return (
-                <SwiperSlide  key={index}>
+        <section className="wellwishers-container">
+          <h2 className="reviews-heading">What our wellwishers say</h2>
+          <Swiper
+            slidesPerView={1}
+            loop={true}
+            autoplay={true} //For autoenabling the swiper functionality with infinite loop
+            speed={2000}
+            spaceBetween={30}
+            pagination={false}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {Testidata.map((item, index) => {
+              //While rendering data on screen return the component with small brackets
+              return (
+                <SwiperSlide key={index}>
                   <div className="hero--wrapper">
                     <div className="parentwrapper">
                       <div className="slidewrapper">
                         <img className="wrapperimg" src={item.image} alt="" />
                         <h5>{item.Name}</h5>
-                        <h5 className="secHeading">{item.Post}</h5>
+                        <h5 className="wisher-secHeading">{item.Post}</h5>
                       </div>
-                      <p className="para"><FaQuoteLeft className="quoteIcon" />{" "}{item.testimony}</p>
+                      <p className="para">
+                        <FaQuoteLeft className="quoteIcon" /> {item.testimony}
+                      </p>
                     </div>
                   </div>
                 </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        <div className="SwiperHeading">
-          <h2 className="reviews--heading">We've worked with</h2>
-        </div>
-        <div className="imagePartners">
-          <Swiper
-            slidesPerView={5}
-            autoplay={{
-              delay: 1000,
-            }}
-            loop={true}
-            speed={3000}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper2"
-          >
-            {Partdata.map((items) => {
-              return (
-                  <SwiperSlide key={items.id}>
-                    <img className="wrapperimg" src={items.Image} alt="" />
-                  </SwiperSlide>
               );
             })}
           </Swiper>
-        </div>
+        </section>
+        <section className="PartnerSwiper-Container">
+          <div className="PartnerSwiper">
+      <div className="SwiperHeading">
+        <h2 className="reviews--heading">We've worked with</h2>
       </div>
+      <div className="imagePartners">
+        <Swiper
+          slidesPerView={6}
+          autoplay={{
+            delay: 1,
+          }}
+          loop={true}
+          speed={1900}
+          spaceBetween={10}
+          pagination={false}
+          modules={[Pagination]}
+          className="mySwiper2"
+        >
+          {Partdata.map((items) => {
+            return (
+              <SwiperSlide key={items.id}>
+                <img className="wrapperimg" src={items.Image} alt="" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+      </div>
+          </section>
     </div>
   );
 };
